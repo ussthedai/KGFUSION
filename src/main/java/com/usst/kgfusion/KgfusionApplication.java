d3db2370e8957680cc45b00bc2fa0dd5f0479893
DaiJianghai <1262381660@qq.com>
2022-09-25 17:32:50 +0800

    war包部署

--- src/main/java/com/usst/kgfusion/KgfusionApplication.java
+++ src/main/java/com/usst/kgfusion/KgfusionApplication.java
@@ -2,12 +2,19 @@ package com.usst.kgfusion;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class KgfusionApplication {
+public class KgfusionApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(KgfusionApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(KgfusionApplication.class);
     }
 
     
