06c7413f258a122c2f2d2ec134deaee294c075e7
ussthedai <113815177+ussthedai@users.noreply.github.com>
2022-09-25 18:32:33 +0800

    war

--- src/main/java/com/usst/kgfusion/KgfusionApplication.java
+++ src/main/java/com/usst/kgfusion/KgfusionApplication.java
@@ -4,12 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class KgfusionApplication {
+public class KgfusionApplication extends SpringBootServletInitializer{
 
     public static void main(String[] args) {
         SpringApplication.run(KgfusionApplication.class, args);
     }
 
-    
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(KgfusionApplication.class);
+    }
 
 }
